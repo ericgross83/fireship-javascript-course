67d8ed1de9f17c191264bf556dfedc1ccc19b376
Eric Gross <eric.gross@volkswagen.de>
2022-11-25 14:21:04 +0100

    Init project

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
-console.log('hi mom ❤️');
 
-const btn = document.querySelector('button')
+let arr = [1,3,5,7]
 
-btn.onclick = () => {
-    alert('you clicked me 🥰');
+
+
+const initialValue = 0;
+const sumWithInitial = arr.reduce(
+  (accumulator, currentValue) => accumulator + currentValue,
+  initialValue
+);
+
+console.log(sumWithInitial);
+
+
+function sum(array) {
+    let total = 0
+
+    array.forEach(element => total = total + element)
+
+    return total
 }
+
+console.log(sum(arr));
+
+arr = ['😊','🦢','🦊','🍇','😏','🙈',]
+
+function search(array, target) {
+
+    return array.findIndex((element) => (element === target))
+}
+
+console.log(search(arr, '🙈')); 
