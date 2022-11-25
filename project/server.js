8aef1121d13c8fb33bd06dba2d70691a72cccea9
Eric Gross <eric.gross@volkswagen.de>
2022-11-25 15:24:42 +0100

    Fix wrong api env key

--- project/server.js
+++ project/server.js
@@ -1,12 +1,10 @@
-import * as dotenv from 'dotenv'
-
-
+import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 dotenv.config()
 
 import {Configuration, OpenAIApi} from 'openai'
 
 const configuration = new Configuration({
-    apiKey: process.env.OpenAIApi
+    apiKey: process.env.OPENAI
 })
 
 const openai = new OpenAIApi(configuration)
